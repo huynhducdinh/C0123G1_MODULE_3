ed929055f4cb061e408ddcc316f1c7a466f28745
huynhducdinh <heeyeon0982002@gmail.com>
2023-04-13 21:25:55 +0700

    main

--- Case Study SQL/tack_2-5.sql
+++ Case Study SQL/tack_2-5.sql
@@ -5,12 +5,11 @@ SELECT *  FROM nhan_vien
 
  
  -- tack 3
- SELECT * FROM khach_hang
+SELECT * FROM khach_hang
 WHERE (YEAR(CURDATE()) - YEAR(ngay_sinh) BETWEEN 18 AND 50) AND (dia_chi LIKE '%Đà Nẵng' OR dia_chi LIKE '%Quảng Trị%');
 										
 -- tack 4
- SELECT * FROM hop_dong;
- SELECT * FROM loai_khach;
+
 SELECT khach_hang.ma_khach_hang,khach_hang.ho_ten, COUNT(ma_hop_dong) AS so_lan_dat_phong
 FROM khach_hang
 JOIN hop_dong ON khach_hang.ma_khach_hang=hop_dong.ma_khach_hang
@@ -20,18 +19,6 @@ GROUP BY ma_khach_hang
 ORDER BY so_lan_dat_phong ;
 
 -- tack 5
-SELECT  khach_hang.ma_khach_hang,
-		khach_hang.ho_ten,
-		loai_khach.ten_loai_khach,
-		hop_dong.ma_hop_dong,
-		dich_vu.ten_dich_vu,
-		hop_dong.ngay_lam_hop_dong,
-		hop_dong.ngay_ket_thuc_hop_dong,(dich_vu.chi_phi_thue)+(hop_dong_chi_tiet.so_luong)*(dich_vu_di_kem.gia) AS tong_tien
-FROM khach_hang
-LEFT JOIN khach_hang ON khach_hang.ma_khach_hang=loai_khach.ma_loai_khach_hang
-ORDER BY ma_khach_hang,ma_hop_dong;
-
-
 
 
 
